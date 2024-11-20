d25efde6a3731e99ce7a53abac2df0041d53f91d
rasel.mahmmud <rasel.mahmud.dev@gmail.com>
2024-11-21 00:04:23 +0600

    added counter loop using java

--- com/example/MyClass.java
+++ com/example/MyClass.java
@@ -1,7 +1,47 @@
 package com.example;
 
-public class MyClass {
-    public static void main(String[] args) {
-        System.out.println("Hello, World! djhksldkfldskfj");
+interface OnEmitListener {
+    void onEmit(Integer playbackTime);
+}
+
+class Player {
+    public Boolean isPlaying = false;
+    public Integer playbackTime = 0;
+    private OnEmitListener emitListener;
+
+    public void setOnEmitListener(OnEmitListener listener) {
+        this.emitListener = listener;
     }
+
+    public void play() {
+        this.isPlaying = true;
+        new Thread(() -> {
+            while (isPlaying) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                playbackTime += 1000;
+                if (emitListener != null) {
+                    emitListener.onEmit(playbackTime);
+                }
+            }
+        }).start();
+    }
+}
+
+public class MyClass {
+
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.play();
+        System.out.println(player.isPlaying);
+
+        player.setOnEmitListener(playbackTime -> {
+            System.out.println("Current playback time: " + playbackTime / 1000 + " seconds");
+        });
+    }
+
+
 }
